aa3dc82a25ad760f6ee598192966fbe4fea3fe39
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:59:38 +0300

    messages

---
+++ 03.Basic-Syntax-Conditional-Statements-And-Loops-More Exercise/05-messages/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05_messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int offset = 0;
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                char mainDigit = command[0];
+                int numberOfDigits = command.Length;
+                offset = (int)(Char.GetNumericValue(mainDigit) - 2) * 3;
+
+                if (mainDigit == '8' || mainDigit == '9')
+                {
+                    offset++;
+                }
+
+                char letter = (char)(97 + offset + numberOfDigits - 1);
+
+                if (mainDigit == '0')
+                {
+                    letter = ' ';
+                }
+
+                result += letter;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
