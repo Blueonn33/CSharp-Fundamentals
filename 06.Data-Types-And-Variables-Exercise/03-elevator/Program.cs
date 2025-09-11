edfaa00a70cbc468333a717474db9c3809e75004
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 22:31:27 +0300

    elevator

---
+++ 06.Data-Types-And-Variables-Exercise/03-elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            double result = 0;
+            
+
+            if(capacity > people)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = (int)(Math.Ceiling((double)people / capacity));
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
