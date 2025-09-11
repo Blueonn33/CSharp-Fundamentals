6110c27059d5a07190012d1d3e43606de21916de
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:25:24 +0300

    back in 30

---
+++ 01.Basic-Syntax-Conditional-Statements-And-Loops-Lab/04-back-in-30-minutes/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04_back_in_30_minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                hour += 1;
+
+                if (hour == 24)
+                {
+                    hour = 0;
+                }
+
+                minutes -= 60;
+            }
+
+            if (minutes < 10)
+            {
+                Console.WriteLine($"{hour}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{minutes}");
+            }
+        }
+    }
+}
