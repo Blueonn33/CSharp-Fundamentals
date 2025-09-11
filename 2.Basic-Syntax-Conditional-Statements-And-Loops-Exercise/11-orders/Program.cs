e222a6df8e3e81c6ceef4a44e677332a4318d3eb
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:51:08 +0300

    orders

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/11-orders/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _11_orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ordersCount = int.Parse(Console.ReadLine());
+            double pricePerCapsule = double.Parse(Console.ReadLine());
+            int daysInMonth = int.Parse(Console.ReadLine());
+            int capsulesCount = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            double currentOrderPrice = 0;
+
+            while (ordersCount > 0)
+            {
+                currentOrderPrice = ((daysInMonth * capsulesCount) * pricePerCapsule);
+                totalPrice += currentOrderPrice;
+                Console.WriteLine($"The price for the coffee is: ${currentOrderPrice:f2}");
+
+                ordersCount--;
+
+                if (ordersCount == 0)
+                {
+                    break;
+                }
+
+                pricePerCapsule = double.Parse(Console.ReadLine());
+                daysInMonth = int.Parse(Console.ReadLine());
+                capsulesCount = int.Parse(Console.ReadLine());
+
+                currentOrderPrice = 0;
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
