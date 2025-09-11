87196e6c349954b2faeb0a7b2ed161e1e022afb4
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 16:20:41 +0300

    chars to string

---
+++ 04.Data-Types-And-Variables-Lab/09-chars-to-string/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _09_chars_to_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+
+            string result = string.Concat(first, second, third);
+            Console.WriteLine(result);
+        }
+    }
+}
