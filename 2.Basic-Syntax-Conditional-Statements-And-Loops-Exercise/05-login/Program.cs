0752cc2d31a688500e6beb7ef1352cd4b17d2e72
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:46:55 +0300

    login

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/05-login/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _05_login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+            bool condition = true;
+            int counter = 0;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            while (condition)
+            {
+                string input = Console.ReadLine();
+
+                if (input == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    condition = false;
+                }
+                else
+                {
+                    counter++;
+
+                    if (counter == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        condition = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect password. Try again.");
+                    }
+                }
+            }
+        }
+    }
+}
