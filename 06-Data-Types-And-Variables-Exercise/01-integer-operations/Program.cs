ad9d5c3b32be9641347b615759f41748a97fa5c6
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 22:17:01 +0300

    integer operations

---
+++ 06-Data-Types-And-Variables-Exercise/01-integer-operations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace _01_integer_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+
+            double result = ((first + second) / third) * fourth;
+            Console.WriteLine(result);
+        }
+    }
+}
