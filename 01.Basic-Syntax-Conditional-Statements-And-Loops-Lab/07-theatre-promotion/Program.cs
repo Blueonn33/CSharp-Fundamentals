c5687347b0ba598ac311231a371b11d549397431
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:27:58 +0300

    theatre

---
+++ 01.Basic-Syntax-Conditional-Statements-And-Loops-Lab/07-theatre-promotion/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _07_theatre_promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                        {
+                            price = 12;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            price = 18;
+                        }
+                        break;
+                    case "Weekend":
+                        if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                        {
+                            price = 15;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            price = 20;
+                        }
+                        break;
+                    case "Holiday":
+                        if (age >= 0 && age <= 18)
+                        {
+                            price = 5;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            price = 12;
+                        }
+                        else if (age > 64 && age <= 122)
+                        {
+                            price = 10;
+                        }
+                        break;
+                }
+                Console.WriteLine($"{price}$");
+            }
+        }
+    }
+}
