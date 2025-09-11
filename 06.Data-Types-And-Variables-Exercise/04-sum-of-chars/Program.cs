76cf9e1be82a0b2d8f543478aabadccaaad8e12b
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 22:33:23 +0300

    sum of chars

---
+++ 06.Data-Types-And-Variables-Exercise/04-sum-of-chars/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_sum_of_chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                char symb = char.Parse(Console.ReadLine());
+                sum += (int)symb;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
