8a3dd50285961d95f55c6919a039dfda6d703d01
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 16:10:50 +0300

    special numbers

---
+++ 04.Data-Types-And-Variables-Lab/05-special-numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05_special_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string result = " ";
+            int sum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                int num = i + 1;
+                while(num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                if(sum == 5 || sum == 7 || sum == 11)
+                {
+                    result = "True";
+                }
+                else
+                {
+                    result = "False";
+                }
+                sum = 0;
+                Console.WriteLine($"{i + 1} -> {result}");
+            }
+        }
+    }
+}
