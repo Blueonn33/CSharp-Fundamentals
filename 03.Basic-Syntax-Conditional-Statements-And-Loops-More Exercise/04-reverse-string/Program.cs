e12c45053bae21d07dd5eb5cea5c76961e98dec7
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:58:54 +0300

    reverse string

---
+++ 03.Basic-Syntax-Conditional-Statements-And-Loops-More Exercise/04-reverse-string/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_reverse_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string reversed = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversed += text[i];
+            }
+
+            Console.WriteLine(reversed);
+        }
+    }
+}
