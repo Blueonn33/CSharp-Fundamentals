64d43458794b48f3a3efc41656b687b7f8f73737
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:47:36 +0300

    strong number

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/06-strong-number/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06_strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int sum = 1;
+            int total = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                for (int j = 1; j <= Char.GetNumericValue(number[i]); j++)
+                {
+                    sum *= j;
+                }
+                total += sum;
+                sum = 1;
+            }
+
+            if (total == int.Parse(number))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
