65417abd6e6ec8fd7d5a141994ddab507a0619cb
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 11:56:03 +0300

    Add project files.

---
+++ 01.Basic-Syntax-Conditional-Statements-And-Loops-Lab/01-student-information/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_student_information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
