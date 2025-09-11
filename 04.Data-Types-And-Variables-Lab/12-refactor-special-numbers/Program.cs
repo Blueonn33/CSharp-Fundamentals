dfe7ab4ec5258d43aa3d0d7c2cf8db28e631c729
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 19:07:49 +0300

    refactor special numbers

---
+++ 04.Data-Types-And-Variables-Lab/12-refactor-special-numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _12_refactor_special_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int sum = 0;
+            bool isSpecialNum = false;
+            for (int i = 1; i <= range; i++)
+            {
+                int number = i;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isSpecialNum);
+                sum = 0;
+            }
+        }
+    }
+}
