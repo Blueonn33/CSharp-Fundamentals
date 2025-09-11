af34c773f1b8e4af8db457388ba704050e7f8861
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 21:15:02 +0300

    decrypting message

---
+++ 05.Data-Types-And -Variables-More-Exercise/05-decrypting-messages/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05_decrypting_messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int interval = int.Parse(Console.ReadLine());
+            string result = "";
+
+            for(int i = 0; i < interval; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                int symbol = (int)input + key;
+                result += (char)symbol;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
