198120776ce0889d54f87c26283c1fba9ea503bf
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:31:57 +0300

    even number

---
+++ 01.Basic-Syntax-Conditional-Statements-And-Loops-Lab/12-even-number/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _12_even_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (number % 2 != 0)
+            {
+                number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please write an even number.");
+            }
+
+            Console.WriteLine("The number is: " + Math.Abs(number));
+        }
+    }
+}
