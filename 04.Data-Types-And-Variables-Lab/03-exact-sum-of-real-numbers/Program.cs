ce4aa9d4c13f12e528db5f820efd07f2d82a2a59
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 15:57:16 +0300

    exact sum

---
+++ 04.Data-Types-And-Variables-Lab/03-exact-sum-of-real-numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03_exact_sum_of_real_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal total = 0;
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                total += number;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
