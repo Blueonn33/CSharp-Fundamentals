12cd8cbaeaade32a7f4593eeb255b8234f93b5fe
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 22:39:09 +0300

    triples

---
+++ 06.Data-Types-And-Variables-Exercise/06-triples-of-latin-letters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _06_triples_of_latin_letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    for(int k = 0; k < n; k++)
+                    {
+                        char first = (char)('a' + i);
+                        char second = (char)('a' + j);
+                        char third = (char)('a' + k);
+                        Console.WriteLine($"{first}{second}{third}");
+                    }
+                }
+            }
+        }
+    }
+}
