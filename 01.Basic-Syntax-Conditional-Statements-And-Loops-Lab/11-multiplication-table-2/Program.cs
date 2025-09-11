7ee43ed9a5131a9562339808863c518ab974619a
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:31:04 +0300

    multiplication table 2

---
+++ 01.Basic-Syntax-Conditional-Statements-And-Loops-Lab/11-multiplication-table-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _11_multiplication_table_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int multiplicator = int.Parse(Console.ReadLine());
+            bool condition = true;
+
+            while (condition)
+            {
+                Console.WriteLine($"{n} X {multiplicator} = {n * multiplicator}");
+                if (multiplicator >= 10)
+                {
+                    condition = false;
+                }
+                else
+                {
+                    multiplicator++;
+                }
+            }
+        }
+    }
+}
