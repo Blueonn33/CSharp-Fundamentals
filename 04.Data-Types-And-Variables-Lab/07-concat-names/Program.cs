2d08663ab1376dbae2dec89c2ae28cc9bb7d84f9
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 16:17:20 +0300

    concat names

---
+++ 04.Data-Types-And-Variables-Lab/07-concat-names/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _07_concat_names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimiter}{secondName}");
+        }
+    }
+}
