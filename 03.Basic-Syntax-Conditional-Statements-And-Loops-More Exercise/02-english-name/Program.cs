edcd782b7bf35828059c3afa507842fe405e5955
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:57:36 +0300

    english name

---
+++ 03.Basic-Syntax-Conditional-Statements-And-Loops-More Exercise/02-english-name/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02_english_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            char symb = number[number.Length - 1];
+            string result = "";
+
+            switch (symb)
+            {
+                case '0':
+                    result = "zero";
+                    break;
+                case '1':
+                    result = "one";
+                    break;
+                case '2':
+                    result = "two";
+                    break;
+                case '3':
+                    result = "three";
+                    break;
+                case '4':
+                    result = "four";
+                    break;
+                case '5':
+                    result = "five";
+                    break;
+                case '6':
+                    result = "six";
+                    break;
+                case '7':
+                    result = "seven";
+                    break;
+                case '8':
+                    result = "eight";
+                    break;
+                case '9':
+                    result = "nine";
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
