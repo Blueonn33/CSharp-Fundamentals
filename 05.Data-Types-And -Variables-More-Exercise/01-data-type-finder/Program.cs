69a6b2bbcea551d330df25ef6eb447573f1b5a90
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 19:38:35 +0300

    data type

---
+++ 05.Data-Types-And -Variables-More-Exercise/01-data-type-finder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+
+namespace _01_data_type_finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while(input != "END")
+            {
+                if (int.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (bool.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (input.Length == 1)
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
