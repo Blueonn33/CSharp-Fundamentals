bef0658cbfdd8de2eae2d981398e253ed5a444bc
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 18:44:04 +0300

    lower / upper

---
+++ 04.Data-Types-And-Variables-Lab/10-lower-or-upper/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10_lower_or_upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if(char.IsUpper(letter))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
