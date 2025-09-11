fbc135fa0fe3ffc34f0474155f416d9218b3c799
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:29:25 +0300

    sum of odd numbers

---
+++ 01.Basic-Syntax-Conditional-Statements-And-Loops-Lab/09-sum-of-odd-numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09_sum_of_odd_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += i * 2 + 1;
+                Console.WriteLine(i * 2 + 1);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
