c9f374e5c72cf5a91da47126cd9c0c455b1610fd
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 12:46:32 +0300

    print and sum

---
+++ 2.Basic-Syntax-Conditional-Statements-And-Loops-Exercise/04-print-and-sum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04_print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+            Console.WriteLine("\nSum: " + sum);
+        }
+    }
+}
