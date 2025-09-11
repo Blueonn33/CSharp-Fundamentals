9179ab256a85a832402a5dc4042e1644fe083b4e
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 21:04:40 +0300

    prime checker

---
+++ 05.Data-Types-And -Variables-More-Exercise/04-refactoring-prime-checker/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04_refactoring_prime_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= range; i++)
+            {
+                string isPrime = "true";
+                for (int divider = 2; divider < i; divider++)
+                {
+                    if (i % divider == 0)
+                    {
+                        isPrime = "false";
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", i, isPrime);
+            }
+        }
+    }
+}
