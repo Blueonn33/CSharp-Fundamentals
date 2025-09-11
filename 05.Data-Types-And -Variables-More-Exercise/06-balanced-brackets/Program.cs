6494fa7d8b01e479539b185133257ae7cbfb7792
Martin Marinov <m.marinov1733@gmail.com>
2025-09-11 21:45:28 +0300

    balanced brackets

---
+++ 05.Data-Types-And -Variables-More-Exercise/06-balanced-brackets/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _06_balanced_brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char openingBracket = '(';
+            char closingBracket = ')';
+            bool isOpened = false;
+            bool isClosed = false;
+            string result = "BALANCED";
+
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+
+                foreach (char symb in command)
+                {
+                    if (symb == openingBracket)
+                    {
+                        if (isOpened)
+                        {
+                            result = "UNBALANCED";
+                        }
+                        else
+                        {
+                            isOpened = true;
+                        }
+                    }
+                    if (symb == closingBracket)
+                    {
+                        isClosed = true;
+                        if (!isOpened)
+                        {
+                            result = "UNBALANCED";
+                        }
+                        else
+                        {
+                            isOpened = false;
+                            isClosed = false;
+                        }
+                    }
+                }
+            }
+
+            if (isOpened && result == "BALANCED")
+            {
+                result = "UNBALANCED";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
