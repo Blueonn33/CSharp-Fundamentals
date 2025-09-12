710012b11c46cd81ddd1d4d9154bcb637a87bc45
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 17:12:14 +0300

    array to num

---
+++ 07.Arrays-Lab/08-condense-array-to-number/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _08_condense_array_to_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int size = nums.Length;
+            int[] condensed = new int[size];
+            int sum = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                if (size == 1)
+                {
+                    Console.WriteLine(nums[0]);
+                    break;
+                }
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    condensed[j] = nums[j] + nums[j + 1];
+                    sum += condensed[j];
+                }
+                nums = condensed;
+                size--;
+            }
+
+            if(nums.Length != 1)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
