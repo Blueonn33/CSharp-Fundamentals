22a1df6dface05f09f419a72932138bb59b56ab8
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 09:48:32 +0300

    beer kegs

---
+++ 06.Data-Types-And-Variables-Exercise/08-beer-kegs/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _08_beer_kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string biggestKeg = "";
+            const double pi = Math.PI;
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = pi * Math.Pow(radius, 2) * height;
+
+                if(volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestKeg = model;
+                }
+            }
+            
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
