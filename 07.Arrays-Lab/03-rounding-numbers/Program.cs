36e081f89cbd2c341ce8fcf3ff58414d3fa3736f
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 16:20:16 +0300

    75/100 - rounding nums

---
+++ 07.Arrays-Lab/03-rounding-numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03_rounding_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            string[] arr = numbers.Split(' ');
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                double num = double.Parse(arr[i]);
+                Console.WriteLine($"{num} => {Math.Round(num, MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
