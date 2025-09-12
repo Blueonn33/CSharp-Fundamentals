804072b264bafa175927f1b70b8dd41b7ec94256
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 10:00:52 +0300

    spice must flow

---
+++ 06.Data-Types-And-Variables-Exercise/09-spice-must-flow/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _09_spice_must_flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int totalSpice = 0;
+
+            while (startingYield >= 100)
+            {
+                days++;
+                totalSpice += startingYield;
+                totalSpice -= 26;
+                startingYield -= 10;
+            }
+
+            Console.WriteLine(days);
+            totalSpice = totalSpice < 26 ? 0 : totalSpice - 26;
+            Console.WriteLine(totalSpice);
+        }
+    }
+}
