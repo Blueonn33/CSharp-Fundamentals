b135dcdbe37ac598d2d95af8b12040d7422f0b55
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 10:17:55 +0300

    pokemon

---
+++ 06.Data-Types-And-Variables-Exercise/10-pokemon/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _10_pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int powerN = int.Parse(Console.ReadLine());
+            int distanceBetweenTargetsM = int.Parse(Console.ReadLine());
+            int exhaustionY = int.Parse(Console.ReadLine());
+
+            int pokedTargets = 0;
+            int initialPower = powerN;
+
+            while(initialPower > distanceBetweenTargetsM)
+            {
+                pokedTargets++;
+                initialPower -= distanceBetweenTargetsM;
+
+                if(initialPower == (powerN / 2) && exhaustionY != 0)
+                {
+                    initialPower /= exhaustionY;
+                }
+            }
+
+            Console.WriteLine(initialPower);
+            Console.WriteLine(pokedTargets);
+        }
+    }
+}
