4b943e448f9ff65b5ccb59bcf5df79c16acac584
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 16:45:44 +0300

    equal arrays

---
+++ 07.Arrays-Lab/07-equal-arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _07_equal_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] second = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            int i = 0;
+            bool identical = true;
+
+            while (identical && i < first.Length)
+            {
+                if (first[i] != second[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    identical = false;
+                }
+                else
+                {
+                    sum += first[i];
+                }
+
+                i++;
+            }
+
+            if(identical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
