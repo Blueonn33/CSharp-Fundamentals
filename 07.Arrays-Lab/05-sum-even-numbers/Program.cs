15f054b306da99a1c43d7e5c6d37a4a1086eda21
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 16:27:00 +0300

    sum even numbers

---
+++ 07.Arrays-Lab/05-sum-even-numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _05_sum_even_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arr = input.Split(' ');
+            int sum = 0;
+
+            foreach(string num in arr)
+            {
+                int n = int.Parse(num);
+
+                if(n % 2 == 0)
+                {
+                    sum += n;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
