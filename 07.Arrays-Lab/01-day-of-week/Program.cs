5b1727444f9b639377a8086ba7484b5b8b667039
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 11:00:35 +0300

    day of week

---
+++ 07.Arrays-Lab/01-day-of-week/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01_day_of_week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day < 8 && day > 0)
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
