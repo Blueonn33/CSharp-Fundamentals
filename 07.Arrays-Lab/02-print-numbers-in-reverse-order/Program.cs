ab7b7ac4f393ae954f6ffe80fe911dac500d1c31
Martin Marinov <m.marinov1733@gmail.com>
2025-09-12 11:10:25 +0300

    reverse order

---
+++ 07.Arrays-Lab/02-print-numbers-in-reverse-order/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02_print_numbers_in_reverse_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            string result = "";
+
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                result += arr[i] + " ";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
