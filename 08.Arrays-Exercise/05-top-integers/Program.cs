3e4f02781ef95f3969e0293520015de0d77b50c7
Martin Marinov <m.marinov1733@gmail.com>
2025-09-15 11:32:19 +0300

    top integers

---
+++ 08.Arrays-Exercise/05-top-integers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _05_top_integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+            List<int> topIntegers = new List<int>();
+
+            int k = 0;
+            bool isTopInteger = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for(int j = i + 1; j < numbers.Length; j++)
+                {
+                    if(numbers[i] > numbers[j])
+                    {
+                        isTopInteger = true;
+                    }
+                    else
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    topIntegers.Add(numbers[i]);
+                    k++;
+                }
+            }
+
+            if(!topIntegers.Contains(numbers[numbers.Length - 1]))
+            {
+                topIntegers.Add(numbers[numbers.Length - 1]);
+            }
+
+            Console.WriteLine(string.Join(" ", topIntegers));
+        }
+    }
+}
