9c7e81806cda5daa40309e4f36f900da19f2b9b4
Martin Marinov <m.marinov1733@gmail.com>
2025-09-15 22:53:34 +0300

    ladybugs

---
+++ 08.Arrays-Exercise/10-ladybugs/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _10_ladybugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] positions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] field = new int[fieldSize];
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                if (positions[i] >= 0 && positions[i] < fieldSize)
+                {
+                    field[positions[i]] = 1;
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandResults = command.Split(' ');
+                int position = int.Parse(commandResults[0]);
+                string direction = commandResults[1];
+                int flyLength = int.Parse(commandResults[2]);
+
+                if (position < 0 || position >= fieldSize || field[position] == 0)
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                field[position] = 0;
+
+                if (direction == "right")
+                {
+                    int newPos = position + flyLength;
+                    while (newPos < fieldSize && field[newPos] == 1)
+                    {
+                        newPos += flyLength;
+                    }
+                    if (newPos < fieldSize)
+                    {
+                        field[newPos] = 1;
+                    }
+                }
+                else if (direction == "left")
+                {
+                    int newPos = position - flyLength;
+                    while (newPos >= 0 && field[newPos] == 1)
+                    {
+                        newPos -= flyLength;
+                    }
+                    if (newPos >= 0)
+                    {
+                        field[newPos] = 1;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
