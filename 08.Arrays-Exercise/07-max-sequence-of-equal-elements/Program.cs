1a0aa2e3a878bc76fed5743acb69b3dccaab4e5e
Martin Marinov <m.marinov1733@gmail.com>
2025-09-15 15:45:47 +0300

    max sequence / magic sum

---
+++ 08.Arrays-Exercise/07-max-sequence-of-equal-elements/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07_max_sequence_of_equal_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            List<int> currentSequence = new List<int> { numbers[0] };
+            List<int> longestSequence = new List<int> { numbers[0] };
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentSequence.Add(numbers[i]);
+                }
+                else
+                {
+                    currentSequence = new List<int> { numbers[i] };
+                }
+
+                if (currentSequence.Count > longestSequence.Count)
+                {
+                    longestSequence = new List<int>(currentSequence);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", longestSequence));
+        }
+    }
+}
---
+++ 08.Arrays-Exercise/08-magic-sum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _08_magic_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                               .Split(' ')
+                               .Select(int.Parse)
+                               .ToArray();
+
+            int magicSum = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                for(int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == magicSum && i != j)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
