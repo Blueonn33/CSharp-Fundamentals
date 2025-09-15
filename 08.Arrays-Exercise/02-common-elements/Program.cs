15b5dcc06e45b787781ce0ec82ac5799736675bc
Martin Marinov <m.marinov1733@gmail.com>
2025-09-15 09:51:03 +0300

    common elements

---
+++ 08.Arrays-Exercise/02-common-elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Immutable;
+
+namespace _02_common_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine().Split(' ');
+            string[] second = Console.ReadLine().Split(' ');
+            int size = second.Length;
+            string[] common = new string[size];
+            int counter = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < first.Length; j++)
+                {
+                    if (second[i] == first[j])
+                    {
+                        common[counter] = second[i];
+                        counter++;
+                        break;
+                    }
+                }
+            }
+
+            for(int i = 0; i < common.Length; i++)
+            {
+                Console.Write(common[i] + " ");
+            }
+        }
+    }
+}
+
+// S of t un i
+// of i 10 un
