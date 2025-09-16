abec78bc9d7009d8558f6dfbc5ffee2fca8857bf
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 14:49:36 +0300

    sign of integer

---
+++ 10.Methods-Lab/01-sign-of-integer-numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01_sign_of_integer_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            checkNumber(number);
+        }
+
+        public static void checkNumber(int number)
+        {
+            string result = number > 0 ? "positive" : number < 0 ? "negative" : "zero";
+            Console.WriteLine($"The number {number} is {result}.");
+        }
+    }
+}
