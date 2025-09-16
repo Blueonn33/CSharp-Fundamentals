78968bd4e63a27de67fb00adb43ce84f13a32e99
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 16:11:09 +0300

    multiply evens by odds

---
+++ 10.Methods-Lab/10-multiply-evens-by-odds/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace _10_multiply_evens_by_odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            getMultipleOfEvenAndOdds(number);
+        }
+
+        public static void getMultipleOfEvenAndOdds(int number)
+        {
+            Console.WriteLine(getSumOfEvenDigits(number) * getSumOfOddDigits(number));
+        }
+
+        public static int getSumOfEvenDigits(int number)
+        {
+            int total = 0;
+
+            while(number > 0)
+            {
+                int digit = number % 10;
+
+                if(digit % 2 == 0)
+                {
+                    total += digit;
+                }
+
+                number /= 10;
+            }
+
+            return total;
+        }
+
+        public static int getSumOfOddDigits(int number)
+        {
+            int total = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+
+                if (digit % 2 != 0)
+                {
+                    total += digit;
+                }
+
+                number /= 10;
+            }
+
+            return total;
+        }
+    }
+}
