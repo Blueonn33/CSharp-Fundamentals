8828260d9f262985cb661b90051fe28d2abea917
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 15:30:34 +0300

    rectangle area

---
+++ 10.Methods-Lab/06-calculate-rectangle-area/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06_calculate_rectangle_area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(area(a, b));
+        }
+
+        public static int area(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
