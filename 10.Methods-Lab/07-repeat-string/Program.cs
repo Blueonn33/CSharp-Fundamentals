167af46349268c50492f863d0fd502bd6c938a7c
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 15:34:30 +0300

    repeat string

---
+++ 10.Methods-Lab/07-repeat-string/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07_repeat_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+
+            repeatText(text, times);
+        }
+
+        public static void repeatText(string text, int times)
+        {
+            string result = "";
+
+            for(int i = 0; i < times; i++)
+            {
+                result += text;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
