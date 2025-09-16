4d3bde329efc200b4c1b185417d33ec289875df8
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 15:28:38 +0300

    order

---
+++ 10.Methods-Lab/05-orders/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05_orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            order(product, quantity);
+        }
+
+        public static void order(string product, int quantity)
+        {
+            double price = 0;
+            switch(product)
+            {
+                case "coffee":
+                    price = 1.5 * quantity;
+                    break;
+                case "water":
+                    price = 1.0 * quantity;
+                    break;
+                case "coke":
+                    price = 1.4 * quantity;
+                    break;
+                case "snacks":
+                    price = 2.0 * quantity;
+                    break;
+            }
+
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
