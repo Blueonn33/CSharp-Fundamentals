ca11fb9a18905f3b31dfa3f96e47da57ddc6acc9
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 16:13:39 +0300

    math operations

---
+++ 10.Methods-Lab/11-math-operations/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _11_math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            string action = Console.ReadLine();
+            int second = int.Parse(Console.ReadLine());
+
+            calculate(action, first, second);
+        }
+
+        public static void calculate(string action, int first, int second)
+        {
+            double result = 0;
+            switch(action)
+            {
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "/":
+                    result = first / second;
+                    break;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
