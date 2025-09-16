82488493c1608e8ebe2deb8497a4b5e7235cced2
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 15:38:07 +0300

    math pow

---
+++ 10.Methods-Lab/08-math-power/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _08_math_power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(calculate(num, power));
+        }
+
+        public static double calculate(double num, int power)
+        {
+            return Math.Pow(num, power);
+        }
+    }
+}
