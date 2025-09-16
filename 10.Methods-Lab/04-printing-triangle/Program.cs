5138cb83c05f3d9acfb49128aebf67714b551abc
Martin Marinov <m.marinov1733@gmail.com>
2025-09-16 15:23:28 +0300

    printing triangle

---
+++ 10.Methods-Lab/04-printing-triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04_printing_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            printTriangle(n);
+        }
+
+        public static void printTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
