ef720ec5a4605b4d1563cbe35c074b32ec8b6963
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 15:39:24 +0300

    add and subtract

---
+++ 11.Methods-Exercise/05-add-and-subtract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+namespace _05_add_and_subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(addSubtract(first, second, third));
+        }
+
+        public static int addSubtract(int first, int second, int third)
+        {
+            return (first + second) - third;
+        }
+    }
+}
