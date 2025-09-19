4011d919bdd6fbf82744b216e1e00deef5c10bec
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 12:50:12 +0300

    smallest number

---
+++ 11.Methods-Exercise/01-smallest-of-three-numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01_smallest_of_three_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            findSmallest(a, b, c);
+        }
+
+        public static void findSmallest(int a, int b, int c)
+        {
+            int first = Math.Min(a, b);
+            int result = Math.Min(c, first);
+            Console.WriteLine(result);
+        }
+    }
+}
