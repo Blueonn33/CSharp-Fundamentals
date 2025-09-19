5830bd2bc1fb4e1808707b5aa6a028bdec9a9ad7
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 16:23:23 +0300

    palindrome integers

---
+++ 11.Methods-Exercise/09-palindrome-integers/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _09_palindrome_integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            palindromeCheck();
+        }
+
+        public static void palindromeCheck()
+        {
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string firstHalf = "";
+                string secondHalf = "";
+
+                if (command.Length == 1)
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    for (int i = 0; i < command.Length / 2; i++)
+                    {
+                        firstHalf += command[i];
+                    }
+
+                    if (command.Length % 2 == 0)
+                    {
+                        for (int i = command.Length - 1; i >= command.Length / 2; i--)
+                        {
+                            secondHalf += command[i];
+                        }
+                    }
+                    else
+                    {
+                        for (int i = command.Length - 1; i > command.Length / 2; i--)
+                        {
+                            secondHalf += command[i];
+                        }
+                    }
+
+                    if (firstHalf == secondHalf)
+                    {
+                        Console.WriteLine(true);
+                    }
+                    else
+                    {
+                        Console.WriteLine(false);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
