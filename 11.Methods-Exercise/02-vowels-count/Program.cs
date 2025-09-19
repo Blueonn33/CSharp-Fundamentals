74ffeca20dfc75ae9421620da68369727f118700
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 12:53:48 +0300

    vowels count

---
+++ 11.Methods-Exercise/02-vowels-count/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02_vowels_count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            printVowels(text);
+        }
+
+        public static void printVowels(string text)
+        {
+            char[] vowels = new char[] { 'a', 'o', 'u', 'e', 'i', 'A', 'O', 'U', 'E', 'I' };
+            int counter = 0;
+
+            foreach(char symbol in text)
+            {
+                if(vowels.Contains(symbol))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
