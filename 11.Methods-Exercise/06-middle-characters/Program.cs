0d9e534f1c0b8ec6d6d91c879c3ab4b1750d900d
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 15:53:00 +0300

    middle chars

---
+++ 11.Methods-Exercise/06-middle-characters/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06_middle_characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            printMiddle(text);
+        }
+
+        public static void printMiddle(string text)
+        {
+            string result = "";
+            int size = text.Length;
+
+            if(size % 2 == 0)
+            {
+                result += text[(size / 2) - 1];
+                result += text[size / 2];
+            }
+            else
+            {
+                result += text[size / 2];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
