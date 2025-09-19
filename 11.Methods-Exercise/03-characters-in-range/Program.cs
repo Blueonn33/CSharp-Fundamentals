88152deee64ff99d475f11bfe80ea43972f34e08
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 12:58:10 +0300

    characters in range

---
+++ 11.Methods-Exercise/03-characters-in-range/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03_characters_in_range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+
+            printChars(start, end);
+        }
+
+        public static void printChars(char start, char end)
+        {
+            if ((int)end < (int)start)
+            {
+                char temp = start;
+                start = end;
+                end = temp;
+            }
+
+            for (int i = (int)start + 1; i < (int)end; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
