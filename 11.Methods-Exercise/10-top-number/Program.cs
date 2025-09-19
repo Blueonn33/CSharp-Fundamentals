6c8c5e2807562007774baecdac0696143078d2b5
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 16:29:40 +0300

    top number

---
+++ 11.Methods-Exercise/10-top-number/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _10_top_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            topNumbers(n);
+        }
+
+        public static void topNumbers(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                int number = i;
+                int sum = 0;
+                bool divisibleBy8 = false;
+                bool containsOdd = false;
+
+                while(number > 0)
+                {
+                    int digit = number % 10;
+                    sum += digit;
+                    number /= 10;
+
+                    if(digit % 2 != 0)
+                    {
+                        containsOdd = true;
+                    }
+                }
+
+                if(sum % 8 == 0 && containsOdd)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
