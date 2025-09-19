fdca1bdafa6824c137fa76611ef4600c0cf40404
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 22:16:39 +0300

    data types

---
+++ 12.Methods-More-Exercise/01-data-types/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string input = Console.ReadLine();
+            checkDataType(dataType, input);
+        }
+
+        public static void checkDataType(string dataType, string input)
+        {
+            switch (dataType)
+            {
+                case "int":
+                    Console.WriteLine(int.Parse(input) * 2);
+                    break;
+                case "real":
+                    Console.WriteLine($"{(float.Parse(input) * 1.5):F2}");
+                    break;
+                case "string":
+                    Console.WriteLine($@"${input}$");
+                    break;
+            }
+        }
+    }
+}
