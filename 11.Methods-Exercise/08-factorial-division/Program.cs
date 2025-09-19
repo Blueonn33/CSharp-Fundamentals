50058b8f1e378202c164d3b046cb5fb0c48af00b
Martin Marinov <m.marinov1733@gmail.com>
2025-09-19 16:05:02 +0300

    factorial division

---
+++ 11.Methods-Exercise/08-factorial-division/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _08_factorial_division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal first = decimal.Parse(Console.ReadLine());
+            decimal second = decimal.Parse(Console.ReadLine());
+
+            decimal result = factorial(first) / factorial(second);
+            Console.WriteLine($"{result:F2}");
+        }
+
+        public static decimal factorial(decimal number)
+        {
+            decimal result = 1;
+
+            while(number > 0)
+            {
+                result *= number;
+                number--;
+            }
+
+            return result;
+        }
+    }
+}
