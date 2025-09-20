e4c47e272183f96371fd8313b1a1e3c4be073c69
Martin Marinov <m.marinov1733@gmail.com>
2025-09-21 00:06:19 +0300

    list of products

---
+++ 13.Lists-Lab/04-list-of-products/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04_list_of_products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            
+            for(int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            list.Sort();
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}.{list[i]}");
+            }
+        }
+    }
+}
