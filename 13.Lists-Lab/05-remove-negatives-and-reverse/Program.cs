25144dd0665aeed75db0715bcadc28adfec6fd7c
Martin Marinov <m.marinov1733@gmail.com>
2025-09-21 00:09:23 +0300

    remove negatives and reverse

---
+++ 13.Lists-Lab/05-remove-negatives-and-reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05_remove_negatives_and_reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> positive = new List<int>();
+
+            for(int i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[i] > 0)
+                {
+                    positive.Add(list[i]);
+                }
+            }
+
+            if(positive.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", positive));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
