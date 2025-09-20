201b481724fc78242a8b0f633805902b802f8e74
Martin Marinov <m.marinov1733@gmail.com>
2025-09-20 22:33:39 +0300

    tribonacci sequence

---
+++ 12.Methods-More-Exercise/04-tribonacci-sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _04_tribonacci_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            tribonacciSequence(n);
+        }
+
+        public static void tribonacciSequence(int n)
+        {
+            List<int> sequence = new List<int> { 1, 1, 2 };
+
+            if (n >= 4)
+            {
+                for (int i = 3; i < n; i++)
+                {
+                    sequence.Add(sequence[i - 1] + sequence[i - 2] + sequence[i - 3]);
+                }
+                Console.WriteLine(string.Join(" ", sequence));
+            }
+            else
+            {
+                for(int i =0; i < n; i++)
+                {
+                    Console.Write(sequence[i] + " ");
+                }
+            }
+            
+        }
+    }
+}
