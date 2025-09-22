01317536721dcc6f1a8dff54b7ca2734d08015b9
Martin Marinov <m.marinov1733@gmail.com>
2025-09-22 19:44:24 +0300

    train

---
+++ 14-Lists-Exercise/01-train/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01_train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> train = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int wagonCapacity = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] symbols = command.Split(' ');
+
+                if (symbols[0] == "Add")
+                {
+                    train.Add(int.Parse(symbols[1]));
+                }
+                else
+                {
+                    for(int i = 0; i < train.Count; i++)
+                    {
+                        if (int.Parse(symbols[0]) + train[i] <= wagonCapacity)
+                        {
+                            train[i] += int.Parse(symbols[0]);
+                            break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", train));
+        }
+    }
+}
