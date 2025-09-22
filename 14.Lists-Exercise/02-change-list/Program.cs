c6ee74c01a2d61f4c18d6811e3619b9f0e1d0536
Martin Marinov <m.marinov1733@gmail.com>
2025-09-22 19:51:33 +0300

    change list

---
+++ 14.Lists-Exercise/02-change-list/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02_change_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+
+            while(command != "end")
+            {
+                string[] symbols = command.Split(' ');
+
+                if (symbols[0] == "Delete")
+                {
+                    numbers.RemoveAll(num => num == int.Parse(symbols[1]));
+                }
+                else if(symbols[0] == "Insert")
+                {
+                    numbers.Insert(int.Parse(symbols[2]), int.Parse(symbols[1]));
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
