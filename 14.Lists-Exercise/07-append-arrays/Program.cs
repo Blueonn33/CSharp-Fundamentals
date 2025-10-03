4d2dadaa08744d3d95271a9944bb70976a9a98f0
Martin Marinov <m.marinov1733@gmail.com>
2025-10-03 23:48:27 +0300

    append arrays

---
+++ 14.Lists-Exercise/07-append-arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07_append_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] parts = input.Split('|');
+            Array.Reverse(parts);
+
+            List<string> result = new List<string>();
+
+            foreach (string part in parts)
+            {
+                string[] numbers = part.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                result.AddRange(numbers);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
