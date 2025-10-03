a2b059d905bd4e72221a14d412c7ecbcec42675c
Martin Marinov <m.marinov1733@gmail.com>
2025-10-04 00:30:38 +0300

    randomize words

---
+++ 16.Objects-And-Classes-Lab/01-randomize-words/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01_randomize_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split();
+            Random random = new Random();
+
+            words = words.OrderBy(w => random.Next()).ToArray();
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
