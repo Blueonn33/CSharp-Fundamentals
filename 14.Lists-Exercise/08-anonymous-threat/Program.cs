2ddde55c73dd0062ac2934896b23e71175e99f1f
Martin Marinov <m.marinov1733@gmail.com>
2025-10-04 00:17:11 +0300

    глупости

---
+++ 14.Lists-Exercise/08-anonymous-threat/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _08_anonymous_threat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] list = input.Split();
+            string[] commands = Console.ReadLine().Split();
+
+            while(commands[0] != "3:1")
+            {
+                if (commands[0] == "merge")
+                {
+
+                }
+            }
+        }
+    }
+}
