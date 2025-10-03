1b7df4e7ebb116c9c9f76f4786f2938716444646
Martin Marinov <m.marinov1733@gmail.com>
2025-10-04 00:34:35 +0300

    big factorial

---
+++ 16.Objects-And-Classes-Lab/02-big-factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace _02_big_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for (int i = number; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
