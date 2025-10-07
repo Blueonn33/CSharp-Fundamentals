7dc8ea90894be959e53f66710da0e0119c25ea31
Martin Marinov <m.marinov1733@gmail.com>
2025-10-07 20:12:20 +0300

    articles 2

---
+++ 17.Objects-And-Classes-Exercise/03-articles-2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _03_articles_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int commands = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+
+            for(int i = 0; i < commands; i++)
+            {
+                string input = Console.ReadLine();
+                string[] getCommands = input.Split(", ");
+
+                Article article = new Article()
+                {
+                    Title = getCommands[0],
+                    Content = getCommands[1],
+                    Author = getCommands[2]
+                };
+
+                articles.Add(article);
+            }
+
+            foreach(Article article in articles)
+            {
+                Console.WriteLine(article.ToString());
+            }
+        }
+    }
+
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public Article()
+        {
+            Title = string.Empty;
+            Content = string.Empty;
+            Author = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
+}
