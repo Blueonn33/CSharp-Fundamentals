f1b59b01bbe19f96116f4a1c3a8bc19b7478ef0d
Martin Marinov <m.marinov1733@gmail.com>
2025-10-07 22:47:42 +0300

    students

---
+++ 17.Objects-And-Classes-Exercise/04-students/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _04_students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+                string[] studentInfo = input.Split(" ");
+                Student student = new Student()
+                {
+                    FirstName = studentInfo[0],
+                    LastName = studentInfo[1],
+                    Grade = double.Parse(studentInfo[2])
+                };
+
+                students.Add(student);
+            }
+
+            List<Student> orderedList = students.OrderByDescending(student => student.Grade).ToList();
+
+            foreach (Student student in orderedList)
+            {
+                Console.WriteLine(student.Display());
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public string Display()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:F2}";
+        }
+    }
+}
