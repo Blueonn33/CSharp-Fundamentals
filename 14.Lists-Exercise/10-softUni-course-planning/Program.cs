2d1921a31dda0fce81ece4081f5832407c004ffc
Martin Marinov <m.marinov1733@gmail.com>
2025-10-09 22:10:49 +0300

    не стана

---
+++ 14.Lists-Exercise/10-softUni-course-planning/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace _10_softUni_course_planning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lessons = Console.ReadLine().Split(", ").ToList();
+            string command = "";
+            List<string> exercises = new();
+
+            while (command != "course start")
+            {
+                command = Console.ReadLine();
+                string[] getCommands = command.Split(":");
+
+                switch (getCommands[0])
+                {
+                    case "Add":
+
+                        if (!lessons.Contains(getCommands[1]))
+                        {
+                            lessons.Add(getCommands[1]);
+                        }
+
+                        break;
+                    case "Insert":
+
+                        if (!lessons.Contains(getCommands[1]))
+                        {
+                            lessons.Insert(int.Parse(getCommands[2]), getCommands[1]);
+                        }
+
+                        break;
+                    case "Remove":
+                        lessons.Remove(getCommands[1]);
+
+                        if (exercises.Contains(getCommands[1]))
+                        {
+                            exercises.Remove(getCommands[1]);
+                            lessons.Remove($"{getCommands[1]}-Exercise");
+                        }
+
+                        break;
+                    case "Swap":
+                        int firstIndex = lessons.FindIndex(l => l == getCommands[1]);
+                        int secondIndex = lessons.FindIndex(l => l == getCommands[2]);
+
+                        string temp = lessons[firstIndex];
+                        lessons[firstIndex] = lessons[secondIndex];
+                        lessons[secondIndex] = temp;
+
+                        if (exercises.Contains(getCommands[1]))
+                        {
+                            lessons.Remove($"{getCommands[1]}-Exercise");
+                            lessons.Insert(secondIndex + 1, $"{getCommands[1]}-Exercise");
+                        }
+                        else if (exercises.Contains(getCommands[2]))
+                        {
+                            lessons.Remove($"{getCommands[2]}-Exercise");
+                            lessons.Insert(firstIndex + 1, $"{getCommands[2]}-Exercise");
+                        }
+
+                        break;
+                    case "Exercise":
+                        if (lessons.Contains(getCommands[1]))
+                        {
+                            int lessonIndex = lessons.FindIndex(l => l == getCommands[1]);
+                            lessons.Insert(lessonIndex + 1, $"{getCommands[1]}-Exercise");
+                        }
+                        else
+                        {
+                            lessons.Add(getCommands[1]);
+                            lessons.Add($"{getCommands[1]}-Exercise");
+                        }
+
+                        exercises.Add(getCommands[1]);
+                        break;
+                }
+            }
+
+            for (int i = 0; i < lessons.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{lessons[i]}");
+            }
+        }
+    }
+}
---
+++ 17.Objects-And-Classes-Exercise/05-teamwork-projects/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace _05_teamwork_projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int teamsCount = int.Parse(Console.ReadLine());
+            List<Team> teams = new List<Team>();
+            List<User> users = new List<User>();
+            Team teamObj = new Team();
+            //John-PowerPuffsCoders
+            for (int i = 0; i < teamsCount; i++)
+            {
+                string command = Console.ReadLine();
+                string[] getTeamInfo = command.Split("-");
+
+                if(teams.Count == 0)
+                {
+                    teamObj.Creator = getTeamInfo[0];
+                    teamObj.Name = getTeamInfo[1];
+                    teams.Add(teamObj);
+                    Console.WriteLine($"Team {teamObj.Name} has been created by {teamObj.Creator}!");
+                }
+                else
+                {
+                    foreach (Team team in teams)
+                    {
+                        if (team.Name == getTeamInfo[0])
+                        {
+                            Console.WriteLine($"Team {team.Name} was already created!");
+                        }
+                        else
+                        {
+
+                            teamObj.Creator = getTeamInfo[0];
+                            teamObj.Name = getTeamInfo[1];
+                            teams.Add(teamObj);
+                            Console.WriteLine($"Team {teamObj.Name} has been created by {teamObj.Creator}!");
+                        }
+                    }
+                }
+
+            }
+
+            string usersCommand = Console.ReadLine();
+
+            while (usersCommand != "end of assignment")
+            {
+                string[] getUserInfo = usersCommand.Split("->");
+                
+                foreach(Team team in teams)
+                {
+                    if(team.Name != getUserInfo[1])
+                    {
+                        Console.WriteLine($"Team {team.Name} does not exist!");
+                    }
+                    else
+                    {
+                        User user = new User()
+                        {
+                            Name = getUserInfo[0]
+                        };
+                        users.Add(user);
+                    }
+                }
+            }
+
+            teamObj.Users = users;
+
+            for(int i = 0; i < teamsCount; i++)
+            {
+
+            }
+        }
+    }
+
+    public class Team
+    {
+        public string Creator { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+
+        public Team()
+        {
+            Creator = string.Empty;
+            Name = string.Empty;
+            Users = new List<User>();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
