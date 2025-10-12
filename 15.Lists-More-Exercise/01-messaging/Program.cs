30cbaee7af293f03eff2b85a8a8f6957341498c2
Martin Marinov <m.marinov1733@gmail.com>
2025-10-12 19:42:14 +0300

    messaging

--- 15.Lists-More-Exercise/01-messaging/Program.cs
+++ 15.Lists-More-Exercise/01-messaging/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string message = Console.ReadLine();
+            List<char> messageChars = message.ToCharArray().ToList();
+            string result = "";
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+                int sumOfDigits = 0;
+
+                while(currentNumber > 0)
+                {
+                    int digit = currentNumber % 10;
+                    sumOfDigits += digit;
+                    currentNumber /= 10;
+                }
+
+                sumOfDigits = sumOfDigits % messageChars.Count;
+                result += messageChars[sumOfDigits];
+                messageChars.RemoveAt(sumOfDigits);
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
