e7e07d26f8f8b9d571b11e0315f87dd8e5c72753
Martin Marinov <m.marinov1733@gmail.com>
2025-10-12 20:19:26 +0300

    car race

---
+++ 15.Lists-More-Exercise/02-car-race/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02_car_race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> times = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            double leftTime = 0;
+            double rightTime = 0;
+
+            for(int i = 0; i < times.Count / 2; i++)
+            {
+                leftTime += times[i];
+
+                if (times[i] == 0)
+                {
+                    leftTime *= 0.8;
+                }
+            }
+
+            for(int i = times.Count - 1; i > times.Count / 2; i--)
+            {
+                rightTime += times[i];
+                if (times[i] == 0)
+                {
+                    rightTime *= 0.8;
+                }
+            }
+
+            if (leftTime < rightTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime.ToString("0.##")}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime.ToString("0.##")}");
+            }
+        }
+    }
+}
