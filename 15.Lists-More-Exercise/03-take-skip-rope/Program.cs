d1c2b38fb9a60e75fee7955a774a0ac25ec7d23d
Martin Marinov <m.marinov1733@gmail.com>
2025-10-15 00:16:10 +0300

    rope , mix

---
+++ 15.Lists-More-Exercise/03-take-skip-rope/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_take_skip_rope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            List<char> symbols = text.ToCharArray().ToList();
+            List<int> numbersList = symbols.Where(s => char.IsDigit(s))
+                .Select(s => int.Parse(s.ToString())).ToList();
+            List<char> nonNumbersList = symbols.Where(s => !char.IsDigit(s)).ToList();
+
+            List<int> takeList = numbersList.Where((n, i) => i % 2 == 0).ToList();
+            List<int> skipList = numbersList.Where((n, i) => i % 2 != 0).ToList();
+
+            string result = "";
+            int index = 0;
+
+            for (int j = 0; j < takeList.Count; j++)
+            {
+                int take = takeList[j];
+                int skip = j < skipList.Count ? skipList[j] : 0;
+
+                for (int t = 0; t < take && index < nonNumbersList.Count; t++)
+                {
+                    result += nonNumbersList[index];
+                    index++;
+                }
+
+                index += skip;
+
+                if (index >= nonNumbersList.Count)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 15.Lists-More-Exercise/04-mixed-up-lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_mixed_up_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+
+            int size = Math.Min(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < size; i++)
+            {
+                result.Add(firstList[i]);
+                result.Add(secondList[secondList.Count - 1 - i]);
+            }
+
+            int a, b;
+            if (firstList.Count > secondList.Count)
+            {
+                a = firstList[firstList.Count - 2];
+                b = firstList[firstList.Count - 1];
+            }
+            else
+            {
+                a = secondList[0];
+                b = secondList[1];
+            }
+
+            int start = Math.Min(a, b);
+            int end = Math.Max(a, b);
+
+            List<int> finalResult = result.Where(x => x > start && x < end).ToList();
+            finalResult.Sort();
+            Console.WriteLine(string.Join(" ", finalResult));
+        }
+    }
+}
