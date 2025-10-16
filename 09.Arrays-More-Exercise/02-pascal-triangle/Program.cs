77dcc36363e3c546841a6814661a48937b83a182
Martin Marinov <m.marinov1733@gmail.com>
2025-10-16 11:35:05 +0300

    pascal triangle

---
+++ 09.Arrays-More-Exercise/02-pascal-triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02_pascal_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int[][] pascalTriangle = new int[numbers][];
+
+            for (int row = 0; row < numbers; row++)
+            {
+                pascalTriangle[row] = new int[row + 1];
+                pascalTriangle[row][0] = 1;
+                pascalTriangle[row][row] = 1;
+
+                for (int col = 1; col < row; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+            }
+
+            foreach (var row in pascalTriangle)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
