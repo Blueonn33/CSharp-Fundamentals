f2c4e36015b84c88c8fffd48d8db45fb62bff85e
Martin Marinov <m.marinov1733@gmail.com>
2025-10-19 09:19:06 +0300

    cooking masterclass

---
+++ 14.Lists-Exercise/09-pokemon-do-not-go/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _09_pokemon_do_not_go
+{
+    internal class Program
+    {
+        /*
+         5 10 6 3 5
+           2
+           4
+           1
+           1
+           3
+           0
+           0
+         */
+        static void Main(string[] args)
+        {
+            List<int> distance = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int sum = 0;
+
+            while (distance.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                if (index >= distance.Count)
+                {
+                    distance.RemoveAt(distance.Count - 1);
+                    distance.Add(distance[0]);
+                    index = distance.Count - 1;
+                }
+
+                int number = distance[index];
+                distance.RemoveAt(index);
+                sum += number;
+
+                for (int i = 0; i < distance.Count; i++)
+                {
+                    if (distance[i] <= number)
+                    {
+                        distance[i] += number;
+                    }
+                    else
+                    {
+                        distance[i] -= number;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Mid-Exam/01-cooking-masterclass/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01_cooking_masterclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceFlour = double.Parse(Console.ReadLine());
+            double priceEgg = double.Parse(Console.ReadLine());
+            double priceApron = double.Parse(Console.ReadLine());
+
+            double sum = 0.0;
+
+            for(int i = 0; i < students; i++)
+            {
+                double appron = priceApron * (students + Math.Ceiling(students * 0.2));
+                double flour = priceFlour * (students - students / 5);
+                double eggs = priceEgg * (students * 10);
+                sum = appron + flour + eggs;
+            }
+
+            if (sum > budget)
+            {
+                Console.WriteLine($"{(sum - budget):F2}$ more needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Items purchased for {sum:F2}$.");
+            }
+        }
+    }
+}
