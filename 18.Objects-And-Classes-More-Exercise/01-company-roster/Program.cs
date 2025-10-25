b0ba22b3972fc491a8d10660e9dfb88712b14687
Martin Marinov <m.marinov1733@gmail.com>
2025-10-25 23:22:31 +0300

    company

---
+++ 18.Objects-And-Classes-More-Exercise/01-company-roster/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _01_company_roster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeCount = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+            decimal highestAverageSalary = 0;
+            string departmentWithHighestAverageSalary = "";
+            decimal departmentAverageSalary = 0;
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                string employeeData = Console.ReadLine();
+                string[] parts = employeeData.Split();
+
+                Employee employee = new Employee(
+                    parts[0],
+                    decimal.Parse(parts[1]),
+                    parts[2]
+                );
+
+                employees.Add(employee);
+            }
+
+            employees.OrderBy(e => e.Department);
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                departmentAverageSalary = employees
+                    .Where(e => e.Department == employees[i].Department)
+                    .Select(e => e.Salary)
+                    .Average();
+
+                if (departmentAverageSalary > highestAverageSalary)
+                {
+                    highestAverageSalary = departmentAverageSalary;
+                    departmentWithHighestAverageSalary = employees[i].Department;
+                }
+            }
+
+            Console.WriteLine($"Highest Average Salary: {departmentWithHighestAverageSalary}");
+            foreach (var employee in employees
+                         .Where(e => e.Department == departmentWithHighestAverageSalary)
+                         .OrderByDescending(e => e.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:F2}");
+            }
+        }
+    }
+}
+
+public class Employee
+{
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+    public string Department { get; set; }
+    public Employee(string name, decimal salary, string department)
+    {
+        Name = name;
+        Salary = salary;
+        Department = department;
+    }
+}
