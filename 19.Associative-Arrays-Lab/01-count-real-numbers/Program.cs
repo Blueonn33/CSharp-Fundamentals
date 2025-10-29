0d650a053a7ede51e83fd6ebc9a7b4d15344cfc6
Martin Marinov <m.marinov1733@gmail.com>
2025-10-29 21:42:26 +0200

    real numbers

---
+++ 19.Associative-Arrays-Lab/01-count-real-numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_count_real_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (double number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach (var kvp in counts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
