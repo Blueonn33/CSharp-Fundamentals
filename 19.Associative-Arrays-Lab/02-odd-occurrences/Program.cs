9b2d1a6de9a910dc036e0aae368cdebae28c9c03
Martin Marinov <m.marinov1733@gmail.com>
2025-10-29 21:47:35 +0200

    odd occurrences

---
+++ 19.Associative-Arrays-Lab/02-odd-occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02_odd_occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToLower().Split();
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordCounts.ContainsKey(word))
+                {
+                    wordCounts[word]++;
+                }
+                else
+                {
+                    wordCounts[word] = 1;
+                }
+            }
+
+            List<string> oddOccurrences = new List<string>();
+
+            foreach (var kvp in wordCounts)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    oddOccurrences.Add(kvp.Key);
+                }
+            }   
+
+            Console.WriteLine(string.Join(" ", oddOccurrences));
+        }
+    }
+}
