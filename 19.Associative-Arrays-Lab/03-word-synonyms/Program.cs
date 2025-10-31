42c158c84b980dc185d5c9171cc436d690fa6207
Martin Marinov <m.marinov1733@gmail.com>
2025-10-31 13:23:14 +0200

    word synonyms

---
+++ 19.Associative-Arrays-Lab/03-word-synonyms/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace _03_word_synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string word = "";
+            string synonym = "";
+
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    word = Console.ReadLine();
+
+                    if (!words.ContainsKey(word))
+                    {
+                        words.Add(word, new List<string>());
+                    }
+                }
+                else
+                {
+                    synonym = Console.ReadLine();
+                    words[word].Add(synonym);
+                }
+            }
+
+            foreach (var w in words)
+            {
+                Console.WriteLine($"{w.Key} - {string.Join(", ", w.Value)}");
+            }
+        }
+    }
+}
