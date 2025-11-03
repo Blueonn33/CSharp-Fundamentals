651faef10a2d36a59fac41f2a0d6a55049435bad
Martin Marinov <m.marinov1733@gmail.com>
2025-11-03 22:56:46 +0200

    count chars

---
+++ 20.Associative-Arrays-Exercise/01-count-chars-in-a-string/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01_count_chars_in_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char s in text)
+            {
+                if (s != ' ')
+                {
+                    if (charCount.ContainsKey(s))
+                    {
+                        charCount[s]++;
+                    }
+                    else
+                    {
+                        charCount[s] = 1;
+                    }
+                }
+            }
+
+            foreach(var (key, value) in charCount)
+            {
+                Console.WriteLine($"{key} -> {value}");
+            }
+        }
+    }
+}
