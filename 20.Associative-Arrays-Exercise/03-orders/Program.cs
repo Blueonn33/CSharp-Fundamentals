4f8aa6a5d5125d96fc234db560b317345424d92d
Martin Marinov <m.marinov1733@gmail.com>
2025-11-03 23:06:39 +0200

    orders

---
+++ 20.Associative-Arrays-Exercise/03-orders/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03_orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string orderInput = "";
+            Dictionary<string, (double price, int quantity)> orders = new Dictionary<string, (double price, int quantity)>();
+
+            while (true)
+            {
+                orderInput = Console.ReadLine();
+
+                if (orderInput == "buy")
+                {
+                    break;
+                }
+
+                string[] orderParts = orderInput.Split(' ');
+                string product = orderParts[0];
+                double price = double.Parse(orderParts[1]);
+                int quantity = int.Parse(orderParts[2]);
+
+                if (orders.ContainsKey(product))
+                {
+                    orders[product] = (price, orders[product].quantity + quantity);
+                }
+                else
+                {
+                    orders[product] = (price, quantity);
+                }
+            }
+
+            foreach (var (product, (price, quantity)) in orders)
+            {
+                Console.WriteLine($"{product} -> {price * quantity:F2}");
+            }
+        }
+    }
+}
