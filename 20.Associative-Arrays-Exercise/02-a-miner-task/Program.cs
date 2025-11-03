58a609ac075fdf8914687f9149fe95598ace3d2b
Martin Marinov <m.marinov1733@gmail.com>
2025-11-03 23:00:45 +0200

    miner

---
+++ 20.Associative-Arrays-Exercise/02-a-miner-task/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02_a_miner_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "stop")
+                {
+                    break;
+                }
+
+                string resource = command;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                {
+                    resources[resource] = quantity;
+                }
+            }
+
+            foreach (var (key, value) in resources)
+            {
+                Console.WriteLine($"{key} -> {value}");
+            }
+        }
+    }
+}
