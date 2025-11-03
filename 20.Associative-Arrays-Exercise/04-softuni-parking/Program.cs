77a67783b33938c78bb4408357bd50e31b621171
Martin Marinov <m.marinov1733@gmail.com>
2025-11-03 23:18:09 +0200

    softuni parking

---
+++ 20.Associative-Arrays-Exercise/04-softuni-parking/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _04_softuni_parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            Dictionary<string, string> parkingLot = new Dictionary<string, string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string command = Console.ReadLine();
+                string[] commandParts = command.Split(' ');
+
+                string action = commandParts[0];
+                string username = commandParts[1];
+
+                if (action == "register")
+                {
+                    string plate = commandParts[2];
+                    if (parkingLot.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {plate}");
+                    }
+                    else
+                    {
+                        parkingLot[username] = plate;
+                        Console.WriteLine($"{username} registered {plate} successfully");
+                    }
+                }
+                else if (action == "unregister")
+                {
+                    if (!parkingLot.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        parkingLot.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var (user, plate) in parkingLot)
+            {
+                Console.WriteLine($"{user} => {plate}");
+            }
+        }
+    }
+}
