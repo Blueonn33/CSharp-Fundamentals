3c444a5d04593ec1bb3562d9f444a1ae087458a3
Martin Marinov <m.marinov1733@gmail.com>
2025-11-04 13:16:52 +0200

    company users

---
+++ 20.Associative-Arrays-Exercise/07-company-users/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _07_company_users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            Dictionary<string, List<string>> companies = new();
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] commandParts = command.Split(" -> ");
+                string companyName = commandParts[0];
+                string employeeId = commandParts[1];
+
+                if (companies.ContainsKey(companyName))
+                {
+                    companies[companyName].Add(employeeId);
+                }
+                else
+                {
+                    companies[companyName] = new List<string>() { employeeId };
+                }
+            }
+
+            foreach( var (companyName, employeeIds) in companies)
+            {
+                Console.WriteLine(companyName);
+                HashSet<string> uniqueEmployeeIds = new HashSet<string>(employeeIds);
+
+                foreach (var employeeId in uniqueEmployeeIds)
+                {
+                    Console.WriteLine($"-- {employeeId}");
+                }
+            }
+        }
+    }
+}
