0e3a18800743b38d3e81a504069eeb344668cd39
Martin Marinov <m.marinov1733@gmail.com>
2025-11-04 12:05:17 +0200

    course

---
+++ 20.Associative-Arrays-Exercise/05-courses/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _05_courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            Dictionary<string, (int studentsCount, List<string> studentNames)> courses = new();
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] commandParts = command.Split(" : ");
+                string courseName = commandParts[0];
+                string studentName = commandParts[1];
+                List<string> studentNames = new();
+                studentNames.Add(studentName);
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = (1, studentNames);
+                }
+                else
+                {
+                    courses[courseName].studentNames.Add(studentName);
+                    courses[courseName] = (courses[courseName].studentsCount + 1, courses[courseName].studentNames);
+                }
+            }
+
+            foreach (var (courseName, studentsCount) in courses)
+            {
+                Console.WriteLine($"{courseName}: {studentsCount.studentsCount}");
+
+                foreach (var student in studentsCount.studentNames)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
