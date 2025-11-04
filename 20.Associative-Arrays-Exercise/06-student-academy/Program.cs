85136c1efa6edbb7246787ad2c8b51959e8b6e89
Martin Marinov <m.marinov1733@gmail.com>
2025-11-04 13:10:14 +0200

    student academy

---
+++ 20.Associative-Arrays-Exercise/06-student-academy/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _06_student_academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<string, (double grade, int gradesCount)> students = new Dictionary<string, (double grade, int gradesCount)>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (students.ContainsKey(name))
+                {
+                    students[name] = (students[name].grade + grade, students[name].gradesCount + 1);
+                }
+                else
+                {
+                    students[name] = (grade, 1);
+                }
+            }
+
+            Dictionary<string, double> averageGrades = new Dictionary<string, double>();
+
+            foreach (var student in students)
+            {
+                double average = student.Value.grade / student.Value.gradesCount;
+
+                if (average >= 4.50)
+                {
+                    averageGrades[student.Key] = average;
+                }
+            }
+
+            foreach (var student in averageGrades)
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+            }
+        }
+    }
+}
