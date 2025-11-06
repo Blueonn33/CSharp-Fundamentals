1e0844ee4c7e8e74ed8cc9c57b650ab4e0c8c667
Martin Marinov <m.marinov1733@gmail.com>
2025-11-06 14:24:23 +0200

    reverse string

---
+++ 22-Text-Processing-Lab/01-reverse-strings/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01_reverse_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            string result = "";
+            Dictionary<string, string> reversedWords = new Dictionary<string, string>();
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    result += command[i];
+                }
+
+                reversedWords.Add(command, result);
+                result = "";
+            }
+
+            foreach (var item in reversedWords)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+        }
+    }
+}
