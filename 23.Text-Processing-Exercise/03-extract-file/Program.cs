6c1eba91f377db7313b1d7c130740fd21f06d5be
Martin Marinov <m.marinov1733@gmail.com>
2025-11-10 13:44:29 +0200

    extract file

---
+++ 23.Text-Processing-Exercise/03-extract-file/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03_extract_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int lastIndexOfSlash = text.LastIndexOf('\\');
+
+            string fileNameWithExtension = text.Substring(lastIndexOfSlash + 1);
+            int lastIndexOfDot = fileNameWithExtension.LastIndexOf('.');
+
+            string fileName = fileNameWithExtension.Substring(0, lastIndexOfDot);
+            string fileExtension = fileNameWithExtension.Substring(lastIndexOfDot + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
