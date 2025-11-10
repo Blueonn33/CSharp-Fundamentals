5b81053b958961e2d2b3d82cbe297138d7ff7258
Martin Marinov <m.marinov1733@gmail.com>
2025-11-10 11:01:33 +0200

    text filter

---
+++ 22-Text-Processing-Lab/04-text-filter/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04_text_filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
