44ddbdd71697502fd40cf34d7c2b4a7e6a5786b6
Martin Marinov <m.marinov1733@gmail.com>
2025-11-10 11:05:39 +0200

    digits, letters and others

---
+++ 22-Text-Processing-Lab/05-digits-letters-and-others/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05_digits_letters_and_others
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            List<char> digits = new List<char>();
+            List<char> letters = new List<char>();
+            List<char> others = new List<char>();
+
+            foreach (char symbol in text)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    digits.Add(symbol);
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    letters.Add(symbol);
+                }
+                else
+                {
+                    others.Add(symbol);
+                }
+            }
+
+            Console.WriteLine(string.Join("", digits));
+            Console.WriteLine(string.Join("", letters));
+            Console.WriteLine(string.Join("", others));
+        }
+    }
+}
