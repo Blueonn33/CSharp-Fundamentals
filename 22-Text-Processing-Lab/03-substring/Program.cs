39b0f619ad75706b0ddbdc589a3cf48f18cff306
Martin Marinov <m.marinov1733@gmail.com>
2025-11-10 10:58:56 +0200

    substring

---
+++ 22-Text-Processing-Lab/03-substring/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03_substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(wordToRemove))
+            {
+                text = text.Remove(text.IndexOf(wordToRemove), wordToRemove.Length);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
