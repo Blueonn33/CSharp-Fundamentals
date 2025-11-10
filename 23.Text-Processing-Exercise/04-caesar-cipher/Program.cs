7be3dc92ba278f17c82847545a61bf66355dc08d
Martin Marinov <m.marinov1733@gmail.com>
2025-11-10 13:46:59 +0200

    caesar cipher

---
+++ 23.Text-Processing-Exercise/04-caesar-cipher/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_caesar_cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string result = "";
+
+            foreach (char symb in text)
+            {
+                result += (char)(symb + 3);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
