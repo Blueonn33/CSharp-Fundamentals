d2f1d6116dc976d0c9e8fbae578e8592d34f923e
Martin Marinov <m.marinov1733@gmail.com>
2025-11-11 11:29:16 +0200

    ascii sumator

---
+++ 24.Text-Processing-More-Exercise/02-ascii-sumator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02_ascii_sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+
+            int sum = 0;
+
+            foreach (char character in text)
+            {
+                if (character > firstChar && character < secondChar)
+                {
+                    sum += character;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
