52ccf1567d539f9c0ee77089adf9fb24521e5310
Martin Marinov <m.marinov1733@gmail.com>
2025-11-11 10:07:13 +0200

    replace chars

--- 23.Text-Processing-Exercise/06-replace-repeating-chars/Program.cs
+++ 23.Text-Processing-Exercise/06-replace-repeating-chars/Program.cs
@@ -1,24 +1,29 @@
-﻿namespace _06_replace_repeating_chars
+﻿using System;
+
+namespace _06_replace_repeating_chars
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string text = Console.ReadLine();
-
-            //foreach (char symb in text)
-            //{
-            //    int lastIndex = text.LastIndexOf(symb);
-
-            //}
+            string result = "";
 
             for (int i = 0; i < text.Length; i++)
             {
                 char symbol = text[i];
-                int lastIndex = text.LastIndexOf(symbol);
+                int j = i + 1;
 
-                //text = text.Replace(new string(symbol, 2));
+                while (j < text.Length && text[j] == symbol)
+                {
+                    j++;
+                }
+
+                result += symbol;
+                i = j - 1;
             }
+
+            Console.WriteLine(result);
         }
     }
 }
