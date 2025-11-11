8b9fa8b9ec8c58ca3ab34123bf6be655b0ed0f05
Martin Marinov <m.marinov1733@gmail.com>
2025-11-11 09:26:31 +0200

    multiply big numbers

---
+++ 23.Text-Processing-Exercise/05-multiply-big-number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace _05_multiply_big_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bigInt = BigInteger.Parse(Console.ReadLine());
+            BigInteger multiplier = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger result = bigInt * multiplier;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 23.Text-Processing-Exercise/06-replace-repeating-chars/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _06_replace_repeating_chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            //foreach (char symb in text)
+            //{
+            //    int lastIndex = text.LastIndexOf(symb);
+
+            //}
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+                int lastIndex = text.LastIndexOf(symbol);
+
+                //text = text.Replace(new string(symbol, 2));
+            }
+        }
+    }
+}
