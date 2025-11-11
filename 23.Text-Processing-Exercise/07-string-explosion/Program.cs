f170176136e91cf16f4377139d3a0d9313933be7
Martin Marinov <m.marinov1733@gmail.com>
2025-11-11 10:46:40 +0200

    string explosion

---
+++ 23.Text-Processing-Exercise/07-string-explosion/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _07_string_explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int explosionStrength = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+
+                if (symbol == '>')
+                {
+                    explosionStrength += int.Parse(text[i + 1].ToString());
+                }
+                else if (explosionStrength > 0)
+                {
+                    text = text.Remove(i, 1);
+                    explosionStrength--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
