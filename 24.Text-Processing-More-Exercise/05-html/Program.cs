874625fad886078d3c08c2e808198482172e8286
Martin Marinov <m.marinov1733@gmail.com>
2025-11-11 23:09:20 +0200

    html

---
+++ 24.Text-Processing-More-Exercise/05-html/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05_html
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string articleTitle = Console.ReadLine();
+            string articleContent = Console.ReadLine();
+            string comment = "";
+
+            string result = $"<h1>\n  {articleTitle}\n</h1>\n" +
+                            $"<article>\n  {articleContent}\n</article>\n";
+
+            while (true)
+            {
+                comment = Console.ReadLine();
+
+                if (comment == "end of comments")
+                {
+                    break;
+                }
+
+                result += $"<div>\n  {comment}\n</div>\n";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
