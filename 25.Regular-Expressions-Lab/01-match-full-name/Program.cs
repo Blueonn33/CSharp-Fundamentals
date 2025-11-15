b7b41c2c907af7f5ab6ad00db3fb09f856eb2f69
Martin Marinov <m.marinov1733@gmail.com>
2025-11-15 18:25:37 +0200

    full name

---
+++ 25.Regular-Expressions-Lab/01-match-full-name/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01_match_full_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            foreach (var match in matches)
+            {
+                Console.Write(match + " ");
+            }
+        }
+    }
+}
---
+++ 25.Regular-Expressions-Lab/02-match-phone-number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_match_phone_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
