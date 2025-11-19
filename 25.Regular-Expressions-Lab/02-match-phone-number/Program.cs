023a4043d10d9d505e1d65652366ab789fec1834
Martin Marinov <m.marinov1733@gmail.com>
2025-11-19 19:42:21 +0200

    phone number

--- 25.Regular-Expressions-Lab/02-match-phone-number/Program.cs
+++ 25.Regular-Expressions-Lab/02-match-phone-number/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace _02_match_phone_number
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02_match_phone_number
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = Console.ReadLine();
+            string pattern = @"\+359(?<separator>[- ])2\k<separator>[\d]{3}\k<separator>[\d]{4}\b";
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            Console.Write(string.Join(", ", matches));
         }
     }
 }
