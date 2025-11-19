618a6ccfb100f1e8bb18d2c258c988e8412ec450
Martin Marinov <m.marinov1733@gmail.com>
2025-11-19 19:48:13 +0200

    dates

---
+++ 25.Regular-Expressions-Lab/03-match-dates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+string pattern = @"\b(?<date>[\d]{2})(?<separator>[\-\/\.])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>[\d]{4})\b";
+
+MatchCollection matches = Regex.Matches(text, pattern);
+
+foreach (Match match in matches)
+{
+    var day = match.Groups["date"].Value;
+    var month = match.Groups["month"].Value;
+    var year = match.Groups["year"].Value;
+
+    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
