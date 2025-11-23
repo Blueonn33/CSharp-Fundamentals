e7f353609f42758b52a2d1b9d615be3689485b07
Martin Marinov <m.marinov1733@gmail.com>
2025-11-23 23:00:28 +0200

    email

---
+++ 26.Regular-Expressions-Exercise/06-extract-emails/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+string pattern =
+    @"(?<=^|\s)" +
+    @"(?<user>[A-Za-z0-9]+(?:[._-][A-Za-z0-9]+)*)" +
+    @"@" +
+    @"(?<host>[A-Za-z]+(?:-[A-Za-z]+)*(?:\.[A-Za-z]+(?:-[A-Za-z]+)*)+)" +
+    @"(?=$|\s|[.,!?])";
+
+MatchCollection matches = Regex.Matches(input, pattern);
+
+foreach (Match match in matches)
+{
+    Console.WriteLine(match.Value);
+}
