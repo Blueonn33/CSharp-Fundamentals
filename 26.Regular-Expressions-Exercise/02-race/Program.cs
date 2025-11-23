b2234927eef7efc0fdc59a16e2ad92a1acfc9227
Martin Marinov <m.marinov1733@gmail.com>
2025-11-23 17:58:26 +0200

    race

---
+++ 26.Regular-Expressions-Exercise/02-race/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+string? names = Console.ReadLine();
+string[] participants = string.IsNullOrWhiteSpace(names)
+    ? Array.Empty<string>()
+    : names.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+Dictionary<string, int> racers = new Dictionary<string, int>();
+
+foreach (string participant in participants)
+{
+    if (!racers.ContainsKey(participant))
+        racers.Add(participant, 0);
+}
+
+while (true)
+{
+    string? command = Console.ReadLine();
+    if (command == null || command == "end of race")
+        break;
+
+    // extract only letters for the name
+    string name = string.Concat(Regex.Matches(command, "[A-Za-z]").Select(m => m.Value));
+
+    // extract only digits and sum them
+    int km = Regex.Matches(command, "[0-9]").Select(m => int.Parse(m.Value)).Sum();
+
+    if (racers.ContainsKey(name))
+    {
+        racers[name] += km;
+    }
+}
+
+var top3 = racers.OrderByDescending(x => x.Value).Take(3).ToList();
+if (top3.Count > 0) Console.WriteLine($"1st place: {top3[0].Key}");
+if (top3.Count > 1) Console.WriteLine($"2nd place: {top3[1].Key}");
+if (top3.Count > 2) Console.WriteLine($"3rd place: {top3[2].Key}");
---
+++ Final-Exam-Preparation/01.secret-chat/Program.cs
@@ -0,0 +1,56 @@
+﻿string message = Console.ReadLine();
+string command = null;
+
+while ((command = Console.ReadLine()) != "Reveal")
+{
+    string[] tokens = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+    string action = tokens[0];
+
+    switch (action)
+    {
+        case "ChangeAll":
+            message = ChangeAll(tokens[1], tokens[2], message);
+            break;
+        case "Reverse":
+            message = Reverse(tokens[1], message);
+            break;
+        case "InsertSpace":
+            break;
+
+    }
+}
+
+Console.WriteLine($"You have a new text message: {message}");
+
+static string ChangeAll(string substring, string replacement, string message)
+{
+    message = message.Replace(substring, replacement);
+
+    Console.WriteLine(message);
+
+    return message;
+}
+
+static string Reverse(string substring, string message)
+{
+    int index = message.IndexOf(substring);
+
+    if(index != -1)
+    {
+        string beforeSubstring = message.Substring(0, index);
+        string afterSubstring = message.Substring(index + substring.Length);
+
+        char[] reversedChars = substring.Reverse().ToArray();
+        string reversed = new string(reversedChars);
+
+        message = beforeSubstring + afterSubstring + reversed;
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+
+    Console.WriteLine(message);
+     
+    return message;
+}
