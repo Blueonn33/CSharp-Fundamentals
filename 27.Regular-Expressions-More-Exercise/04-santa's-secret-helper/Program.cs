0de2937caf97132fd82b9c30886ed6fa59417d8f
Martin Marinov <m.marinov1733@gmail.com>
2025-11-24 16:12:22 +0200

    santa

---
+++ 27.Regular-Expressions-More-Exercise/04-santa's-secret-helper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        int key = int.Parse(Console.ReadLine());
+        string line;
+        string pattern = @"@(?<name>[A-Za-z]+)[^@\-!:>]*!(?<behavior>[GN])!";
+
+        List<string> goodChildren = new List<string>();
+
+        while ((line = Console.ReadLine()) != "end")
+        {
+            string decrypted = string.Concat(line.Select(ch => (char)(ch - key)));
+
+            Match match = Regex.Match(decrypted, pattern);
+            if (match.Success && match.Groups["behavior"].Value == "G")
+            {
+                goodChildren.Add(match.Groups["name"].Value);
+            }
+        }
+
+        foreach (string child in goodChildren)
+        {
+            Console.WriteLine(child);
+        }
+    }
+}
