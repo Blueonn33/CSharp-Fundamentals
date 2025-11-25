9f955da0ed547b52972648b72e3465306053d777
Martin Marinov <m.marinov1733@gmail.com>
2025-11-25 12:04:20 +0200

    pokemon dont go

--- 14.Lists-Exercise/09-pokemon-do-not-go/Program.cs
+++ 14.Lists-Exercise/09-pokemon-do-not-go/Program.cs
@@ -1,38 +1,45 @@
-﻿namespace _09_pokemon_do_not_go
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_pokemon_do_not_go
 {
     internal class Program
     {
-        /*
-         5 10 6 3 5
-           2
-           4
-           1
-           1
-           3
-           0
-           0
-         */
         static void Main(string[] args)
         {
-            List<int> distance = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            List<int> distance = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToList();
+
             int sum = 0;
 
             while (distance.Count > 0)
             {
                 int index = int.Parse(Console.ReadLine());
+                int number;
 
-                if (index >= distance.Count)
+                if (index < 0)
                 {
+                    number = distance[0];
+                    sum += number;
+                    distance.RemoveAt(0);
+                    distance.Insert(0, distance[distance.Count - 1]);
+                }
+                else if (index >= distance.Count)
+                {
+                    number = distance[distance.Count - 1];
+                    sum += number;
                     distance.RemoveAt(distance.Count - 1);
                     distance.Add(distance[0]);
-                    index = distance.Count - 1;
                 }
-
-                int number = distance[index];
-                distance.RemoveAt(index);
-                sum += number;
+                else
+                {
+                    number = distance[index];
+                    sum += number;
+                    distance.RemoveAt(index);
+                }
 
                 for (int i = 0; i < distance.Count; i++)
                 {
