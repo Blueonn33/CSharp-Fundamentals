a4b82d193560491999db97098d1bce3ec906b00c
Martin Marinov <m.marinov1733@gmail.com>
2025-11-25 11:48:52 +0200

    longest sequence

--- 09.Arrays-More-Exercise/05-longest-increasing-subsequence/Program.cs
+++ 09.Arrays-More-Exercise/05-longest-increasing-subsequence/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string numbers = Console.ReadLine();
+            int[] sequence = numbers.Split(' ').Select(int.Parse).ToArray();
+
+            List<int> longestIncreasingSubsequence = FindLongestIncreasingSubsequence(sequence);
+            Console.WriteLine(string.Join(" ", longestIncreasingSubsequence));
+        }
+
+        static List<int> FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            int n = sequence.Length;
+            int[] lengths = new int[n];
+            int[] previous = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                lengths[i] = 1;
+                previous[i] = -1;
+            }
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (sequence[i] > sequence[j] && lengths[i] < lengths[j] + 1)
+                    {
+                        lengths[i] = lengths[j] + 1;
+                        previous[i] = j;
+                    }
+                }
+            }
+            int maxLengthIndex = Array.IndexOf(lengths, lengths.Max());
+            List<int> lis = new List<int>();
+            while (maxLengthIndex != -1)
+            {
+                lis.Add(sequence[maxLengthIndex]);
+                maxLengthIndex = previous[maxLengthIndex];
+            }
+            lis.Reverse();
+            return lis;
         }
     }
 }
