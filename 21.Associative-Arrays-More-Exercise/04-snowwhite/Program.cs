ab47f0e109ff19781303c877bcba2621a5ce0870
Martin Marinov <m.marinov1733@gmail.com>
2025-11-29 16:28:01 +0200

    snowwhite

--- 21.Associative-Arrays-More-Exercise/04-snowwhite/Program.cs
+++ 21.Associative-Arrays-More-Exercise/04-snowwhite/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace _04_snowwhite
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_snowwhite
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var dwarfs = new List<(string Name, string HatColor, int Physics)>();
+            string command;
+
+            while ((command = Console.ReadLine()) != "Once upon a time")
+            {
+                string[] dwarfInfo = command.Split(" <:> ");
+                string name = dwarfInfo[0];
+                string hatColor = dwarfInfo[1];
+                int physics = int.Parse(dwarfInfo[2]);
+
+                var existing = dwarfs.FirstOrDefault(d => d.Name == name && d.HatColor == hatColor);
+                if (existing != default)
+                {
+                    if (physics > existing.Physics)
+                    {
+                        dwarfs.Remove(existing);
+                        dwarfs.Add((name, hatColor, physics));
+                    }
+                }
+                else
+                {
+                    dwarfs.Add((name, hatColor, physics));
+                }
+            }
+
+            var ordered = dwarfs
+                .OrderByDescending(d => d.Physics)
+                .ThenByDescending(d => dwarfs.Count(x => x.HatColor == d.HatColor))
+                .ToList();
+
+            foreach (var dwarf in ordered)
+            {
+                Console.WriteLine($"({dwarf.HatColor}) {dwarf.Name} <-> {dwarf.Physics}");
+            }
         }
     }
 }
