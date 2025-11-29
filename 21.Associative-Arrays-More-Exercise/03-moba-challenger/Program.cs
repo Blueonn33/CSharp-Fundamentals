29b57b76e982aa37745f7c1107e4f7b5c25ff26f
Martin Marinov <m.marinov1733@gmail.com>
2025-11-29 21:06:58 +0200

    moba

--- 21.Associative-Arrays-More-Exercise/03-moba-challenger/Program.cs
+++ 21.Associative-Arrays-More-Exercise/03-moba-challenger/Program.cs
@@ -1,5 +1,6 @@
-﻿using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03_moba_challenger
 {
@@ -7,63 +8,61 @@ namespace _03_moba_challenger
     {
         static void Main(string[] args)
         {
-            string command = "";
-            Dictionary<string, Dictionary<string, int>> players = new();
+            var players = new Dictionary<string, Dictionary<string, int>>();
+            string input;
 
-            while ((command = Console.ReadLine()) != "Season end")
+            while ((input = Console.ReadLine()) != "Season end")
             {
-                string[] playerInput = command.Split(" -> ");
-                string name = playerInput[0];
-                string role = playerInput[1];
-                int skill = int.Parse(playerInput[2]);
-
-                if (playerInput[1] == "vs")
+                if (input.Contains("->"))
                 {
-                    if (!players.ContainsKey(playerInput[0]) && !players.ContainsKey(playerInput[2]))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        string roles = "";
+                    string[] parts = input.Split(" -> ");
+                    string player = parts[0];
+                    string position = parts[1];
+                    int skill = int.Parse(parts[2]);
 
-                        foreach (var player in players)
+                    if (!players.ContainsKey(player))
+                        players[player] = new Dictionary<string, int>();
+
+                    if (!players[player].ContainsKey(position))
+                        players[player][position] = skill;
+                    else if (players[player][position] < skill)
+                        players[player][position] = skill;
+                }
+                else if (input.Contains("vs"))
+                {
+                    string[] parts = input.Split(" vs ");
+                    string first = parts[0];
+                    string second = parts[1];
+
+                    if (players.ContainsKey(first) && players.ContainsKey(second))
+                    {
+                        var common = players[first].Keys.Intersect(players[second].Keys);
+                        if (common.Any())
                         {
-                            string playerName = player.Key;
-                            string player1 = playerInput[0];
+                            int firstTotal = players[first].Values.Sum();
+                            int secondTotal = players[second].Values.Sum();
 
-                            //players[player1]
-
-                            //Dictionary<string, int> attributes = players[player1].Value;
-                            string player2 = playerInput[2];
-
-                            if (playerName == player1)
-                            {
-                                //Dictionary<string, int> attributes = player.Value;
-
-                                //foreach (var attribute in attributes)
-                                //{
-                                //    string attributeName = attribute.Key;
-                                //    int attributeValue = attribute.Value;
-                                //}
-                            }
-
-                            
-
+                            if (firstTotal > secondTotal)
+                                players.Remove(second);
+                            else if (secondTotal > firstTotal)
+                                players.Remove(first);
                         }
                     }
                 }
-                else
+            }
+
+            foreach (var player in players
+                .OrderByDescending(p => p.Value.Values.Sum())
+                .ThenBy(p => p.Key))
+            {
+                int total = player.Value.Values.Sum();
+                Console.WriteLine($"{player.Key}: {total} skill");
+
+                foreach (var pos in player.Value
+                    .OrderByDescending(p => p.Value)
+                    .ThenBy(p => p.Key))
                 {
-                    if (!players.ContainsKey(name))
-                    {
-                        players[name] = new Dictionary<string, int>();
-                        players[name].Add(role, skill);
-                    }
-                    else
-                    {
-                        players[name].Add(role, skill);
-                    }
+                    Console.WriteLine($"- {pos.Key} <::> {pos.Value}");
                 }
             }
         }
