c308da8bb1cca5f3df5a99dd4451fa79f63128a1
Martin Marinov <m.marinov1733@gmail.com>
2025-11-29 21:08:33 +0200

    ranking

--- 21.Associative-Arrays-More-Exercise/01-ranking/Program.cs
+++ 21.Associative-Arrays-More-Exercise/01-ranking/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace _01_ranking
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_ranking
 {
     internal class Program
     {
@@ -6,42 +10,56 @@
         {
             Dictionary<string, string> contestPasswords = new();
             Dictionary<string, Dictionary<string, int>> userContests = new();
-            string input = "";
 
-            while (true)
+            string input;
+
+            while ((input = Console.ReadLine()) != "end of contests")
             {
-                input = Console.ReadLine();
-
-                if (input == "end of contests")
-                {
-                    break;
-                }
-
-                string[] contestData = input.Split(":");
-                string contestName = contestData[0];
-                string contestPassword = contestData[1];
-
-                contestPasswords[contestName] = contestPassword;
+                string[] data = input.Split(':');
+                string contest = data[0];
+                string password = data[1];
+                contestPasswords[contest] = password;
             }
 
-            while (true)
+            while ((input = Console.ReadLine()) != "end of submissions")
             {
-                input = Console.ReadLine();
+                string[] data = input.Split("=>");
+                string contest = data[0];
+                string password = data[1];
+                string username = data[2];
+                int points = int.Parse(data[3]);
 
-                if (input == "end of submissions")
+                if (contestPasswords.ContainsKey(contest) &&
+                    contestPasswords[contest] == password)
                 {
-                    break;
+                    if (!userContests.ContainsKey(username))
+                        userContests[username] = new Dictionary<string, int>();
+
+                    if (!userContests[username].ContainsKey(contest))
+                        userContests[username][contest] = points;
+                    else if (userContests[username][contest] < points)
+                        userContests[username][contest] = points;
                 }
+            }
 
-                string[] submissionData = input.Split("=>");
-                string contestName = submissionData[0];
-                string contestPassword = submissionData[1];
-                string username = submissionData[2];
-                int points = int.Parse(submissionData[3]);
+            string bestUser = userContests
+                .OrderByDescending(u => u.Value.Values.Sum())
+                .First().Key;
 
-                if (contestPasswords.ContainsKey(contestName) && contestPasswords[contestName] == contestPassword)
+            int bestPoints = userContests[bestUser].Values.Sum();
+
+            Console.WriteLine($"Best candidate is {bestUser} with total {bestPoints} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var user in userContests.OrderBy(u => u.Key))
+            {
+                Console.WriteLine(user.Key);
+
+                foreach (var contest in user.Value
+                    .OrderByDescending(c => c.Value)
+                    .ThenBy(c => c.Key))
                 {
-                    
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
                 }
             }
         }
