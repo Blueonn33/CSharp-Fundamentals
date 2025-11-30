280541bad8a50ee1c3d0c8e92a550442edd70b0e
Martin Marinov <m.marinov1733@gmail.com>
2025-11-30 15:45:48 +0200

    degustation party

---
+++ Final-Exam/03-degustation-party/Program.cs
@@ -0,0 +1,55 @@
+﻿Dictionary<string, List<string>> guests = new();
+int dislikedMealsCount = 0;
+string command = "";
+
+while ((command = Console.ReadLine()) != "Stop")
+{
+    string[] commandParts = command.Split('-', StringSplitOptions.RemoveEmptyEntries);
+    string action = commandParts[0];
+    string guestName = commandParts[1];
+    string meal = commandParts[2];
+
+    if (action == "Like")
+    {
+        if (!guests.ContainsKey(guestName))
+        {
+            guests[guestName] = new List<string>();
+            guests[guestName].Add(meal);
+        }
+        else
+        {
+            if (!guests[guestName].Contains(meal))
+            {
+                guests[guestName].Add(meal);
+            }
+        }
+    }
+    else if (action == "Dislike")
+    {
+        if (!guests.ContainsKey(guestName))
+        {
+            Console.WriteLine($"{guestName} is not at the party.");
+        }
+        else
+        {
+            if (!guests[guestName].Contains(meal))
+            {
+                Console.WriteLine($"{guestName} doesn't have the {meal} in his/her collection.");
+            }
+            else
+            {
+                guests[guestName].Remove(meal);
+                dislikedMealsCount++;
+                Console.WriteLine($"{guestName} doesn't like the {meal}.");
+            }
+        }
+    }
+}
+
+foreach (var kvp in guests)
+{
+    Console.Write($"{kvp.Key}: ");
+    Console.WriteLine(string.Join(", ", kvp.Value));
+}
+
+Console.WriteLine("Unliked meals: " + dislikedMealsCount);
